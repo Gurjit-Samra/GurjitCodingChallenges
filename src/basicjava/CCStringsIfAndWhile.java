506c7e2d93eca8a9587d3962081dcf5c3910c243
Gurjit Samra <gurjit.samra1@ucalgary.ca>
2023-02-18 21:12:47 +0000

    Implemented the method "count" and passed all relevant JUnit tests

--- src/basicjava/CCStringsIfAndWhile.java
+++ src/basicjava/CCStringsIfAndWhile.java
@@ -16,10 +16,33 @@ public class CCStringsIfAndWhile {
 		return isDigit;
 	}
 
-	public static int count(String string, String string2) {
+	public static int count(String string1, String string2) {
 		// TODO Auto-generated method stub
-		return 0;
-	}
+		int indexCountString2 = 0;
+		int occurences = 0; 
+		int lenString2 = string2.length();
+		int lenString1 = string1.length();
+		System.out.println(lenString2);
+		while (indexCountString2 < lenString2) {
+			String charForCompare = (string2.substring(indexCountString2,
+					(indexCountString2 + 1))).toLowerCase();
+			int indexCountString1 = 0;
+			while(indexCountString1 < lenString1) {
+			String charToCompare = (string1.substring(indexCountString1,
+					(indexCountString1 + 1))).toLowerCase();
+			if(charForCompare.compareTo(charToCompare)== 0) {
+				occurences += 1;
+				indexCountString1 += 1;
+			}else {
+				indexCountString1 += 1;
+			}
+			
+			}
+			indexCountString2 += 1;
+			}
+
+		return occurences;
+		}
 
 	public static int smallestDigit(int i) {
 		// TODO Auto-generated method stub
