4a2f8ebee322c32bb8cbcb842203683daadc9378
Gurjit <Gurjit@GSamra-PC>
2023-02-23 17:49:39 -0700

    Implemented "replace" method and passed all relevant JUnit tests.

--- src/basicjava/CCArrays.java
+++ src/basicjava/CCArrays.java
@@ -1,8 +1,26 @@
 package basicjava;
 
 public class CCArrays {
-	
+	/**
+	 * method "replace" with the following parameters replaces the chosen char with a given char.  
+	 * @param charArray
+	 * @param toReplace
+	 * @param replaceWith
+	 */
 	 public static void replace(char[] charArray, char toReplace, char replaceWith) {
+		 int lengthOfArray = charArray.length;
+		 String letterToReplace = (Character.toString(toReplace)).toLowerCase();
+		 int index = 0;
+		 while (index < lengthOfArray) {
+			 String currentLetter = (Character.toString(charArray[index])).toLowerCase();
+			 if ( currentLetter.compareTo(letterToReplace) == 0) {
+				 charArray[index] = replaceWith;
+			 }
+			 index++;
+		 }
+		 
+		 System.out.print(charArray);
+		 
 		 
 	 }
 	 
