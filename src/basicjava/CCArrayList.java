7c0649791d64400683692a80010edb63a02604ca
Gurjit <Gurjit@GSamra-PC>
2023-02-24 19:02:24 -0700

    Implemented "indexOfIgnoreCase" method and passed all relevant JUnit tests.

--- src/basicjava/CCArrayList.java
+++ src/basicjava/CCArrayList.java
@@ -6,12 +6,25 @@ public class CCArrayList {
 
 	public static int indexOfIgnoreCase(ArrayList<String> strs, String string) {
 		// TODO Auto-generated method stub
-		return 0;
+		int indexOfMatchingString = -1;
+		int arrayListLength = strs.size();
+		
+		for (int i = 0; i < arrayListLength; i++) {
+			String stringAtIndex = strs.get(i);
+			strs.remove(i);
+			strs.add(i, stringAtIndex.toLowerCase());
+		}
+		
+		for(int index = 0; index < arrayListLength; index ++) {
+			if(strs.contains(string.toLowerCase())) {
+				indexOfMatchingString = strs.indexOf(string.toLowerCase());
+			}
+		}
+		return indexOfMatchingString;
 	}
 
 	public static void insert(ArrayList<Double> nums, double d, int i) {
 		// TODO Auto-generated method stub
-		
 	}
 
 }
