3ad81b2b9be212f9a348a2b05625e86b6a8ec542
Gurjit <Gurjit@GSamra-PC>
2023-02-24 19:09:27 -0700

    Implemented "insert" method,
    w/o loops and passed all relevant JUnit tests.

--- src/basicjava/CCArrayList.java
+++ src/basicjava/CCArrayList.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 public class CCArrayList {
 
 	public static int indexOfIgnoreCase(ArrayList<String> strs, String string) {
-		// TODO Auto-generated method stub
 		int indexOfMatchingString = -1;
 		int arrayListLength = strs.size();
 		
@@ -24,7 +23,9 @@ public class CCArrayList {
 	}
 
 	public static void insert(ArrayList<Double> nums, double d, int i) {
-		// TODO Auto-generated method stub
+		if(!(i > nums.size())) {
+			nums.add(i, d);
+		}
 	}
 
 }
