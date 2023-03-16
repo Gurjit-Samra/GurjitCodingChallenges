190c992708e946fdff6ed8263c256fcc66078be0
Gurjit <Gurjit@GSamra-PC>
2023-03-15 19:59:31 -0600

    Created skeleton for new Flight Class

---
+++ src/oosequence/Flight.java
@@ -0,0 +1,32 @@
+package oosequence;
+
+import java.util.Date;
+/**
+ * 
+ * Class representing a flight
+ *
+ */
+public class Flight {	
+Date departure;
+Date arrival;
+
+/**
+ * Javadoc for constructor
+ * @param departureDate
+ * @param arrivalDate
+ */
+public Flight(Date departure, Date arrival) {
+	
+}
+
+public Flight(Flight other) {
+	
+}
+
+long length() {
+	
+	return 0;
+}
+
+
+}
