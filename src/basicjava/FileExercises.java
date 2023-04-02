3238214d1dec91db10c9469790634d9097ab8ab6
Gurjit <Gurjit@GSamra-PC>
2023-04-02 16:34:47 -0600

    Created skeleton of FileExcercies class

---
+++ src/basicjava/FileExercises.java
@@ -0,0 +1,18 @@
+package basicjava;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class FileExercises {
+
+	public static int counting(String string) throws FileNotFoundException, IOException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public void toUpper(String string, String string2) throws FileNotFoundException, IOException{
+		// TODO Auto-generated method stub
+		
+	}
+
+}
