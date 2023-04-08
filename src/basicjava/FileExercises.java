9faa570b51a75c28167972b22b0728e6862f9e5a
Gurjit <Gurjit@GSamra-PC>
2023-04-08 12:17:58 -0600

    Implemented toUpper method in FileExercises
    class, and passed all JUnit test

--- src/basicjava/FileExercises.java
+++ src/basicjava/FileExercises.java
@@ -1,9 +1,12 @@
 package basicjava;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class FileExercises {
 	
@@ -41,19 +44,31 @@ public class FileExercises {
 	static int subStringCounter(String string , String stringToCount) {
 		
 		if (string.contains(stringToCount)) {
-			System.out.print("\nCounting " + "'" + stringToCount + "'" + " in " + string);
+			//System.out.print("\nCounting " + "'" + stringToCount + "'" + " in " + string);
 			return 1 + subStringCounter(string.replaceFirst(stringToCount, ""), stringToCount);	
 		}
 		return 0;
 	}
 
 	
-	public void toUpper(String string, String string2) throws FileNotFoundException, IOException{
+	public void toUpper(String filename, String outputFileName) throws FileNotFoundException, IOException{
+		BufferedReader reader = new BufferedReader(new FileReader(filename));
+		
+		BufferedWriter writer = new BufferedWriter(new FileWriter(outputFileName));
+		PrintWriter output = new PrintWriter(writer);
+		
+		for( String line ; (line = reader.readLine()) != null; ) {
+			 String upperCaseLine = line.toUpperCase();
+			 System.out.print(upperCaseLine);
+			 //writer.write(upperCaseLine);
+			 output.println(upperCaseLine);
+			 }
+		
+		reader.close();
+		writer.close();
+		 }
 		// the output file should be created and contain exactly the same 
 		//content as the input file, except that everything in the output file is in upper case. So, if the 
 		//input file contained the text “Coding Challenge” then the output file should contain the text 
-		//“CODING CHALLENGE”
-		
+		//“CODING CHALLENGE”	
 	}
-
-}
