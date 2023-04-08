5013922f72e31f1fdd03d04f4a0d58a1dedd6c16
Gurjit <Gurjit@GSamra-PC>
2023-04-08 00:26:39 -0600

    Implemented method counter and passed required JUnit tests

--- src/basicjava/FileExercises.java
+++ src/basicjava/FileExercises.java
@@ -1,17 +1,58 @@
 package basicjava;
 
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 
 public class FileExercises {
-
-	public static int counting(String string) throws FileNotFoundException, IOException {
-		// TODO Auto-generated method stub
+	
+	/**
+	 * Reads a file a counts how many times the stringToCount is repeated
+	 * @param stringToCount
+	 * @return
+	 * @throws FileNotFoundException
+	 * @throws IOException
+	 */
+	public static int counting(String stringToCount) throws FileNotFoundException, IOException {
+		 BufferedReader reader = new BufferedReader(new FileReader("example.txt"));
+		 int stringCount = 0;		 
+		 
+		 for( String line ; (line = reader.readLine()) != null; ) {
+			 String lowercaseLine = line.toLowerCase();
+			// System.out.print(lowercaseLine);
+			 if(lowercaseLine.contains(stringToCount.toLowerCase())) {
+				 //System.out.print(stringCount);
+				 stringCount += subStringCounter(lowercaseLine , stringToCount.toLowerCase());
+				// System.out.print(stringCount);
+			 }
+		 }
+		 reader.close();
+		 return stringCount;
+	}
+	
+	/**
+	 * Recursively counts occurrences of stringToCount 
+	 * 
+	 * @param string
+	 * @param stringToCount
+	 * @return
+	 */
+	static int subStringCounter(String string , String stringToCount) {
+		
+		if (string.contains(stringToCount)) {
+			System.out.print("\nCounting " + "'" + stringToCount + "'" + " in " + string);
+			return 1 + subStringCounter(string.replaceFirst(stringToCount, ""), stringToCount);	
+		}
 		return 0;
 	}
 
+	
 	public void toUpper(String string, String string2) throws FileNotFoundException, IOException{
-		// TODO Auto-generated method stub
+		// the output file should be created and contain exactly the same 
+		//content as the input file, except that everything in the output file is in upper case. So, if the 
+		//input file contained the text “Coding Challenge” then the output file should contain the text 
+		//“CODING CHALLENGE”
 		
 	}
 
